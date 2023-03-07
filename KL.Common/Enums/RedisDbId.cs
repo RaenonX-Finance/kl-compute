243dee5934d5a762affd6e67fb4bc6ee014d67e1
RaenonX <raenonx0710@gmail.com>
2023-03-06 21:18:48 -0600

    Added Redis DB ID for Socket.IO clustering
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- KL.Common/Enums/RedisDbId.cs
+++ KL.Common/Enums/RedisDbId.cs
@@ -1,6 +1,12 @@
-﻿namespace KL.Common.Enums;
+﻿using JetBrains.Annotations;
+
+namespace KL.Common.Enums;
 
 
 public enum RedisDbId {
-    LastPxAndMomentum = 0
+    LastPxAndMomentum = 0,
+
+    // Added to note that the database is occupied
+    [UsedImplicitly]
+    SocketIoCluster = 8
 }
